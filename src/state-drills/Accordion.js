5b522a48ce821599a397367e9f6ecaa5c9ae0f1b
Nishat Hoque <hoquen0397@gmail.com>
2021-02-19 18:23:45 -0500

    Accordion Component

---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class Accordion extends Component {
+    static defaultProps = {
+        sections: []
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            currentSectionIndex: 0
+        }
+    }
+
+    handleButtonClicked = (index) => {
+        this.setState({
+            currentSectionIndex: index
+        })
+    }
+    
+    renderButton(){
+        return this.props.sections.map((sections, index) => (
+            <li>
+                <button 
+                    key ={index}
+                    onClick = {() => this.handleButtonClicked(index)}
+                >
+                    {sections.title}
+                </button> 
+                {index === this.state.currentSectionIndex ?this.renderContent(): ''}
+            </li> 
+        ))
+    }
+
+    renderContent(){
+        const currentSection = this.props.sections[this.state.currentSectionIndex]
+        return (
+            <div className = 'content'>
+                {currentSection.content}
+            </div>
+        )
+    }
+    render() { 
+        return (
+            <div className = 'Accordion'>
+                <ul>
+                    {this.renderButton()}
+                </ul>
+            </div>
+        )
+    }
+}
+ 
+export default Accordion;
