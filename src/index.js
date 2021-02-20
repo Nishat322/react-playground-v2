c4a70320fb9e6c242109b9b6308ffc37ff7a911c
Nishat Hoque <hoquen0397@gmail.com>
2021-02-20 16:01:36 -0500

    paltrows

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
 //import App from './App'
 //import App from './composition/App'
 //import App from './messages/App'
 //import App from './state/App'
 //import App from './state-drills/App'
-import App from './demonymapp/App'
+//import App from './demonymapp/App'
+import App from './paltrows-power-toes-setup/src/App'
 import './index.css'
 
 ReactDOM.render(
-  <App/> , document.getElementById('root')
+  <BrowserRouter>
+     <App/> 
+  </BrowserRouter>
+  , document.getElementById('root')
 )
