c5f6b21be9dfa8237f42c73e0fbd5e4f2ff4d99c
Nishat Hoque <hoquen0397@gmail.com>
2021-02-21 12:50:45 -0500

    registration form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+//import {BrowserRouter} from 'react-router-dom'
 //import App from './App'
 //import App from './composition/App'
 //import App from './messages/App'
@@ -8,12 +8,11 @@ import {BrowserRouter} from 'react-router-dom'
 //import App from './state-drills/App'
 //import App from './demonymapp/App'
 //import App from './paltrows-power-toes-setup/src/App'
-import App from './lang-context/App'
+//import App from './lang-context/App'
+import RegistrationForm from './register/RegistrationForm'
 import './index.css'
 
 ReactDOM.render(
-  <BrowserRouter>
-     <App/> 
-  </BrowserRouter>
+  <RegistrationForm/>
   , document.getElementById('root')
 )
